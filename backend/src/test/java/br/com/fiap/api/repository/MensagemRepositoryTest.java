aad174f88eccc6ada60917bcf4ef607401697c69
eugabiviana <gabi.viana88@gmail.com>
2023-12-20 15:39:18 -0300

    Criando testes nÃao implementados

--- backend/src/test/java/br/com/fiap/api/repository/MensagemRepositoryTest.java
+++ backend/src/test/java/br/com/fiap/api/repository/MensagemRepositoryTest.java
@@ -1,4 +1,29 @@
 package br.com.fiap.api.repository;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.fail;
+
 public class MensagemRepositoryTest {
+
+    @Test
+    void devePermitirRegistrarMensagem(){
+        fail("Teste não implementado!");
+    }
+
+    @Test
+    void devePermitirAlterarMensagem(){
+        fail("Teste não implementado!");
+    }
+
+    @Test
+    void devePermitirExcluirMensagem(){
+        fail("Teste não implementado!");
+    }
+
+    @Test
+    void devePermitirBuscarMensagem(){
+        fail("Teste não implementado!");
+    }
 }
