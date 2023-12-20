e1b842b91c690be5a649880a1d6f276f9586943f
eugabiviana <gabi.viana88@gmail.com>
2023-12-20 17:15:00 -0300

    Implementando o testes devePermitirBuscarMensagem e devePermitirExcluirMensagem

--- backend/src/test/java/br/com/fiap/api/repository/MensagemRepositoryTest.java
+++ backend/src/test/java/br/com/fiap/api/repository/MensagemRepositoryTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -33,11 +34,7 @@ public class MensagemRepositoryTest {
     @Test
     void devePermitirRegistrarMensagem(){
         //Arrange
-        var mensagem = Mensagem.builder()
-                .id(UUID.randomUUID())
-                .usuario("Dani")
-                .conteudo("counteúdo da msg")
-                .build();
+        var mensagem = gerarMensagem();
 
         when(mensagemRepository.save(any(Mensagem.class))).thenReturn(mensagem);
         //Act
@@ -46,23 +43,45 @@ public class MensagemRepositoryTest {
         //Assert
         assertThat(mensagemRegistrada).isNotNull().isEqualTo(mensagem);
         verify(mensagemRepository, times(1)).save(any(Mensagem.class));
-
-
     }
 
     @Test
     void devePermitirBuscarMensagem(){
-        fail("Teste não implementado!");
-    }
+        //Arrange
+        var id = UUID.randomUUID();
+        var mensagem = gerarMensagem();
+        mensagem.setId(id);
 
-    @Test
-    void devePermitirAlterarMensagem(){
-        fail("Teste não implementado!");
+        when(mensagemRepository.findById(any(UUID.class)))
+                .thenReturn(Optional.of(mensagem));
+        //Act
+        var mensagemRecebidaOpcional = mensagemRepository.findById(id);
+
+        //Assert
+        assertThat(mensagemRecebidaOpcional).isPresent().containsSame(mensagem);
+        mensagemRecebidaOpcional.ifPresent(mensagemRecebida -> {
+            assertThat(mensagemRecebida.getId()).isEqualTo(mensagem.getId());
+            assertThat(mensagemRecebida.getConteudo()).isEqualTo(mensagem.getConteudo());
+        });
+        verify(mensagemRepository, times(1)).findById(any(UUID.class));
     }
 
     @Test
     void devePermitirExcluirMensagem(){
-        fail("Teste não implementado!");
+        //Arrange
+       var id = UUID.randomUUID();
+       doNothing().when(mensagemRepository).deleteById(any(UUID.class));// doNothing -> porque o delete não tem retorno
+        //Act
+        mensagemRepository.deleteById(id);
+        //Assert
+        verify(mensagemRepository, times(1)).deleteById(any(UUID.class));
+
+    }
+    private Mensagem gerarMensagem(){
+        return Mensagem.builder()
+                .usuario("Dani")
+                .conteudo("counteúdo da msg")
+                .build();
     }
 
 
